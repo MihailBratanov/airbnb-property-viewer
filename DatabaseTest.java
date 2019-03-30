1b5e0c8995bc5e9fcd92a1ee96503ffd8cde8979
Sin, Ka Wang <ka.sin@kcl.ac.uk>
2019-03-30 01:27:31 +0000

    DatabaseTest class update.

--- DatabaseTest.java
+++ DatabaseTest.java
@@ -1,40 +1,56 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
-class DatabaseTest {
-
+/**
+ * Test class for Airbnb Property Viewer.
+ * This class tests for the class Database of the project.
+ * 
+ * @author Haiyun Zou, Ka Wang Sin, Mihail Bratanov and Terry Phung
+ * @version 2019.03.29
+ *
+ * 18-19 4CCS1PPA Programming Practice and Applications
+ * Term 2 Coursework 4 - London Property Marketplace
+ * Created by Haiyun Zou, Ka Wang Sin, Mihail Bratanov and Terry Phung
+ * Student ID: 1828556, 1850162, 1838362, 1833386
+ * k-number: k1895418, k1802265, k1888765, k1895389
+ *
+ */
+public class DatabaseTest
+{
     private Database databaseTest = new Database();
     private ArrayList actualDatabaseEntries = new ArrayList();
 
-
-    @org.junit.jupiter.api.Test
-    void getDatabaseEntries() {
+    @Test
+    public void getDatabaseEntries() {
         UserDetails user = new UserDetails("crystal", "zou", "czou123","czou123123");
         UserDetails mdb1 = new UserDetails("Mihail", "Bratanov", "mdb","mdb123456");
         actualDatabaseEntries.add(user);
         assertEquals(databaseTest.getDatabaseEntries().get(0).toString(), actualDatabaseEntries.get(0).toString());
     }
-
-    @org.junit.jupiter.api.Test
-    void getUserProfile() {
-        assertEquals(databaseTest.getUserProfile("czou123").get("Camden"),2);
-        assertEquals(databaseTest.getUserProfile("czou123").get("Bromley"),0);
-        assertEquals(databaseTest.getUserProfile("czou123").get("Kensington and Chelsea"),1);
-
+    
+    @Test
+    public void getUserProfile() {
+        assertEquals(databaseTest.getUserProfile("czou123").get("Camden").toString(),"2");
+        assertEquals(databaseTest.getUserProfile("czou123").get("Bromley").toString(), "0");
+        assertEquals(databaseTest.getUserProfile("czou123").get("Kensington and Chelsea").toString(),"1");
     }
-
-    @org.junit.jupiter.api.Test
-    void getUserDetails() {
+    
+    @Test
+    public void getUserDetails() {
         String[] expected = new String[]{"crystal", "zou", "czou123"};
         assertEquals(databaseTest.getUserDetails("czou123")[0].toString(), expected[0].toString());
         assertEquals(databaseTest.getUserDetails("czou123")[1].toString(), expected[1].toString());
         assertEquals(databaseTest.getUserDetails("czou123")[2].toString(), expected[2].toString());
     }
-
-    @org.junit.jupiter.api.Test
-    void getMostClickedBorough() {
+    
+    @Test
+    public void getMostClickedBorough() {
         assertEquals(databaseTest.getMostClickedBorough("czou123"),"Camden");
     }
+
 }
+
